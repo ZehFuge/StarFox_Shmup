95dc652799eff6c6fceddd56d8eb34039502ce39
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-10-19 22:34:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ pygame.init()
 # set mainloop var
 running = True
 
+# set last_update for time events
+last_update = pygame.time.get_ticks()
 
 # enemy respawn variable for testing
-respawn = 10
+respawn = 35
 
 # main loop block
 while running:
@@ -25,13 +27,17 @@ while running:
         if event.type == pygame.QUIT:
             GS.end_game()
 
-    # update sprites
+    # update / move sprites
     GS.all_sprites.update()
 
 
     # check sprite collision
     # collision player by meteors
     hits = pygame.sprite.spritecollide(GS.player, GS.meteors, True, pygame.sprite.collide_circle)
+    # player loses 1 life, also the hurt_mode is set to false to advoid the impact death
+    if hits:
+        GS.player.hurt()
+
     # if enemy dies through hit, raise respawn variable
     for hit in hits:
         respawn += 1
@@ -40,6 +46,7 @@ while running:
     hits = pygame.sprite.groupcollide(GS.meteors, GS.bullets, True, True)
     # if enemy dies through hit, raise respawn variable
     for hit in hits:
+        GS.player.score += (100 - hit.radius) * GS.player.score_multiplier
         respawn += 1
 
 
@@ -60,8 +67,11 @@ while running:
     # needs to be drawn last, to stay on top layer
     # draw_lives(surface, x, y, lives, img)
     GS.draw_lives(GS.screen, GS.WIDTH - 100, 5, GS.player.lives, GS.player_live_img)
+    # draw score surface
+    GS.draw_score()
     # draw_text(surface, text, size, x, y)
 
 
     # after drawing, flip (CAF.GS).screen
     pygame.display.flip()
+    pygame.display.update()
