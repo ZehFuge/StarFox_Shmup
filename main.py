0ed90ce7dccb5af443e8f68d4d314eb6e6497b6d
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-10-21 21:42:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # library import block
 import pygame
 from game import *
+import random
 
 
 # init block
@@ -15,7 +16,7 @@ running = True
 last_update = pygame.time.get_ticks()
 
 # enemy respawn variable for testing
-respawn = 35
+respawn = 15
 
 # main loop block
 while running:
@@ -27,11 +28,17 @@ while running:
         if event.type == pygame.QUIT:
             GS.end_game()
 
+    # check if player lost live and shall not be drawn
+    if GS.player.hide:
+        GS.all_sprites.remove(GS.player)
+    if not GS.player.hide:
+        GS.all_sprites.add(GS.player)
+
     # update / move sprites
     GS.all_sprites.update()
 
 
-    # check sprite collision
+    # check sprite collisions
     # collision player by meteors
     hits = pygame.sprite.spritecollide(GS.player, GS.meteors, True, pygame.sprite.collide_circle)
     # player loses 1 life, also the hurt_mode is set to false to advoid the impact death
@@ -42,12 +49,18 @@ while running:
     for hit in hits:
         respawn += 1
 
+
     # collision meteor by bullets
     hits = pygame.sprite.groupcollide(GS.meteors, GS.bullets, True, True)
     # if enemy dies through hit, raise respawn variable
     for hit in hits:
         GS.player.score += (100 - hit.radius) * GS.player.score_multiplier
         respawn += 1
+        # 5% chance to drop power up by meteor kill
+        if random.random() > 0.95:
+            pow = GS.Power_Ups(hit.rect.center[0], hit.rect.center[1])
+            GS.all_sprites.add(pow)
+            GS.power_ups.add(pow)
 
 
     # enemy respawn loop
@@ -58,6 +71,13 @@ while running:
         respawn -= 1
 
 
+    # collision player by power up
+    hits = pygame.sprite.groupcollide(GS.power_ups, GS.players, True, False)
+    for hit in hits:
+        if hit.type == "wings":
+            GS.player.power_level += 1
+
+
     # draw / render sprites
     # GS.screen.fill(GS.BLACK)
     GS.screen.blit(GS.background_img, GS.background_img_rect)
@@ -66,7 +86,9 @@ while running:
     # draw / render game interface
     # needs to be drawn last, to stay on top layer
     # draw_lives(surface, x, y, lives, img)
-    GS.draw_lives(GS.screen, GS.WIDTH - 100, 5, GS.player.lives, GS.player_live_img)
+    GS.draw_lives(GS.screen, GS.WIDTH / 2, 5, GS.player.lives, GS.player_live_img)
+    # draw the players hp bar
+    GS.draw_shield_bar(GS.screen, GS.WIDTH - 210, 10, GS.player.shield)
     # draw score surface
     GS.draw_score()
     # draw_text(surface, text, size, x, y)
