e3d63a54e4c23e970c4d1c847fd7549d7c9dfa17
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-11-05 20:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,117 +50,13 @@ while running:
     GS.all_sprites.update()
 
 
-    # check sprite collisions
-    # player hit by others
-    # collision player by meteors
-    hits = pygame.sprite.spritecollide(GS.player, GS.meteors, True, pygame.sprite.collide_circle)
-    # player loses 1 life, also the hurt_mode is set to false to advoid the impact death
-    for hit in hits:
-        # create explosion image
-        explosion = GS.Explosion(hit.rect.center, "lg")
-        GS.all_sprites.add(explosion)
-
-        # hurt the player
-        GS.player.hurt()
-    # if meteor dies through hit, raise respawn variable
-    for hit in hits:
-        respawn += 1
-
-
-    # collision player by enemy
-    hits = pygame.sprite.spritecollide(GS.player, GS.enemys, True, pygame.sprite.collide_circle)
-    # player loses 1 life, also the hurt_mode is set to false to advoid the impact death
-    for hit in hits:
-        # create explosion image
-        explosion = GS.Explosion(hit.rect.center, "lg")
-        GS.all_sprites.add(explosion)
-
-        # hurt the player
-        GS.player.hurt()
-    # if enemy dies through hit, raise respawn variable
-    for hit in hits:
-        respawn += 1
-
-
-    # collision player by enemy_bullets
-    hits = pygame.sprite.spritecollide(GS.player, GS.enemy_bullets, True)
-    # remove player HP if hit
-    for hit in hits:
-        # create explosion image
-        explosion = GS.Explosion(hit.rect.center, "sm")
-        GS.all_sprites.add(explosion)
-
-        # hurt the player
-        GS.player.hurt()
-
-
-    # collision meteor by bullets
-    hits = pygame.sprite.groupcollide(GS.meteors, GS.bullets, True, True)
-    # if meteor dies through hit, raise respawn variable
-    for hit in hits:
-        # create explosion image and play its sound
-        explosion = GS.Explosion(hit.rect.center, "lg")
-        GS.all_sprites.add(explosion)
-        GS.explosion_sound.play()
-
-        # calculate new player score
-        GS.player.score += (100 - hit.radius) * GS.player.score_multiplier
-        respawn += 1
-        # 5% chance to drop power up by meteor kill
-        if random.random() >= 0.95:
-            pow = GS.Power_Ups(hit.rect.center[0], hit.rect.center[1])
-            GS.all_sprites.add(pow)
-            GS.power_ups.add(pow)
-
-
-    # collision enemys by bullets
-    hits = pygame.sprite.groupcollide(GS.enemys, GS.bullets, True, True)
-    # if enemy dies through hit, raise respawn variable
-    for hit in hits:
-        # create explosion image and play its sound
-        explosion = GS.Explosion(hit.rect.center, "lg")
-        GS.all_sprites.add(explosion)
-        GS.explosion_sound.play()
-
-        # calculate new player score
-        GS.player.score += 100 * GS.player.score_multiplier
-        respawn += 1
-        # 5% chance to drop power up by enemy kill
-        if random.random() > 0.95:
-            pow = GS.Power_Ups(hit.rect.center[0], hit.rect.center[1])
-            GS.all_sprites.add(pow)
-            GS.power_ups.add(pow)
-
     # check if enemys need to be respawned
-    if respawn > 0:
-        respawn = GS.generate_villans(respawn)
+    # if respawn > 0:
+    respawn = GS.generate_villans(respawn)
 
 
-    # collision player by power up
-    hits = pygame.sprite.groupcollide(GS.power_ups, GS.players, True, False)
-    for hit in hits:
-        # enhence the quantity of players lasers
-        if hit.type == "wings":
-            GS.power_up_sound["wings"].play()
-            GS.player.power_level += 1
-        # raise the multiplicator score of the player
-        if hit.type == "double":
-            GS.power_up_sound["double"].play()
-            GS.player.score_multiplier += 1
-        # heal player for a little bit
-        if hit.type == "silver":
-            GS.power_up_sound["rings"].play()
-            GS.player.shield += 15
-            # check if players life raised above 100 and correct it
-            if GS.player.shield > 100:
-                GS.player.shield = 100
-        # heal player for a big amount
-        if hit.type == "gold":
-            GS.power_up_sound["rings"].play()
-            GS.player.shield += 50
-            # check if players life raised above 100 and correct it
-            if GS.player.shield > 100:
-                GS.player.shield = 100
+    # check sprite collisions
+    respawn = GS.collision_check()
 
 
     # draw / render sprites
