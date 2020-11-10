9560190e16d5bc94bc59e462e706363fa82dc433
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-11-10 20:30:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ game_over_screen = False
 
 # set last_update for time events
 last_update = {}
-last_update["game_start"] = pygame.time.get_ticks()
 
 
 # store amount for respawn of villans
 respawn = 0
+allow_respawn = True
 
 
 # needed to spawn creatures at the beginning of the game
@@ -36,14 +36,6 @@ start_music = False
 while running:
     # keep the loop running at the right speed
     GS.clock.tick(GS.FPS)
-
-    if GS.player.hurt_mode \
-        and not killed_spawn:
-        respawn = GS.killer.kill_and_count()
-        killed_spawn = True
-
-    if GS.player.hurt_mode:
-        killed_spawn = False
 
     # check if player died
     if GS.player.lives == 0:
@@ -66,7 +58,40 @@ while running:
         start_music = True
         startmenu_screen = False
 
-    # wait for few seconds for villans to spawn and music to play
+    # destroy all sprites, if player got killed and save their amount
+    # also forbid enemy spawning
+    # killed_spawn allows the statement to be executed only once
+    if not GS.player.hurt_mode \
+        and not killed_spawn:
+        # save the amount of from screen cleared villans
+        respawn = GS.killer.kill_and_count()
+
+        # check if respawn amount is to overwhelming for the player
+        if respawn >= 20:
+            respawn = 10
+
+        # save the time of the player death
+        last_update["got_killed"] = pygame.time.get_ticks()
+
+        # change killed_spawn to denied second execute of this if statement
+        killed_spawn = True
+
+        # forbid enemy spawning
+        allow_respawn = False
+
+    now = pygame.time.get_ticks()
+    # allow to spawn villans again
+    # 1 second after player recovered from lifeloss
+    if GS.player.hurt_mode \
+            and killed_spawn \
+            and (now - last_update["got_killed"]) >= (GS.player.hurt_delay + 1000):
+        # 1 second after player spawn, change killed spawn to be able to execute again, if player dies again
+        killed_spawn = False
+
+        # allow enemy spawning
+        allow_respawn = True
+
+    # wait few seconds for villans to spawn and music to play
     # also good luck sound can ring through
     # and the player can be prepared for the enemys
     now = pygame.time.get_ticks()
@@ -76,6 +101,7 @@ while running:
         GS.jukebox("game")
         start_music = False
 
+    # allow enemy spawning 3 seconds after game start
     if not startmenu_screen \
         and start_spawn \
         and (now - last_update["game_start"]) > 3000:
@@ -88,17 +114,23 @@ while running:
         if event.type == pygame.QUIT:
             GS.end_game()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                GS.pause_menu()
+
 
     # update / move sprites
     GS.all_sprites.update()
 
 
     # check if enemys need to be respawned
-    respawn = GS.generate_villans(respawn)
+    if allow_respawn:
+        respawn = GS.generate_villans(respawn)
 
 
     # check sprite collisions
-    respawn = GS.collision_check()
+    if allow_respawn:
+     respawn = GS.collision_check()
 
 
     # draw / render sprites
